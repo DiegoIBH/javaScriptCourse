c466f84184643ceaceb920f4a2ca159e484568e7
DiegoIBH <diegobarrosh1163@gmail.com>
2024-03-12 11:37:18 +0100

    Scrap Udemy course

---
+++ jsPractice.js
@@ -0,0 +1,30 @@
+//Primera definicion de variable
+let miNumero1 = 15;
+console.log(typeof miNumero1);
+// y /* */ se utiliza para hacer notas en multipleas lineas
+
+/* Tipos de datos que para definir varialbes
+    Number(numerico y flotante), String, Boolean (true or false), Null(ausencia de referenia) and undefine(ausencia de valor) */
+
+let miString = "hola";
+let miBoolean = true;
+let miNull = null;
+let miUndefined = undefined;
+//Definir el dato y el tipo de dato
+console.log(miNull);
+console.log(typeof miNull);
+
+//Hoisting - con "Var" Se declara e inicializa una variable por separado y te permite inicializar antes de declarar (lo cual no es recomedable) y "let" no te lo permite... lo cual es buena practica. 
+let x;
+x = 10;
+console.log(x);
+
+//Definir constante (NO CAMBIA) usando mayusculas y _ para separar palabras
+const MI_CONST = 10;
+//MI_CONST = 20; este cambio generara un error en consola
+console.log(MI_CONST);
+// la const suele ser para numero predeterminados, como seg en un min y el valor de PI.
+console.log(Math.PI);
+
+
+
---
+++ scrapFiles/jsScrapFiles.js
@@ -0,0 +1,22 @@
+/*Operadores: Aritmeticos, Asignacion, Logicos y Comparacion
+Aritmeticos: + - * / %(residuo division) **Potencia ++ Incremento -- Decremento */
+
+let a,b,c,d,e,f;
+//suma
+a = 3+4;
+console.log(a);
+//resta
+b = 3-2;
+console.log(b);
+//multip
+c = 6*8;
+console.log(c);
+//divi
+d = c/4;
+console.log(d);
+// % residuo de division (cuantas veces cabe )
+e = 206%5;
+console.log(e);
+// **
+f = 2**3;
+console.log(f);
